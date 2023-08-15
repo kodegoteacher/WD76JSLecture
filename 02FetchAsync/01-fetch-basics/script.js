6a551796fd2ac65ec3c953ed6c74de3ef7c6cc4e
kodego teacher <kodegoteacher@gmail.com>
2023-08-14 22:38:50 -0700

    JS Lectures

--- 02FetchAsync/01-fetch-basics/script.js
+++ 02FetchAsync/01-fetch-basics/script.js
@@ -1,1 +1,43 @@
 //fetch api it returns a promise,
+//######################################
+//accessing the movies.json using fetch
+// fetch("./movies.json").then((response) => {
+//   console.log(response);
+// })
+
+//######################################
+//fetching the json file
+
+// fetch("./movies.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+
+//######################################
+//fetching from a text file
+
+// fetch("./test.txt")
+//   .then((response) => response.text())
+  //   .then((data) => console.log(data))
+
+  //######################################
+  //Fetching form an API
+
+  // fetch("https://api.github.com/users")
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data))
+  
+  //individual users, put them on the window (DOM)
+  
+    fetch("https://api.github.com/users/kodegoteacher")
+    .then((response) => response.json())
+    .then((data) => (document.querySelector("h1").textContent = data.login))
+
+//######################################
+//######################################
+//######################################
+//######################################
+//######################################
+//######################################
+//######################################
+//######################################
+//######################################
